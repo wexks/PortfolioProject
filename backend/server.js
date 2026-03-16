ad325afdc3f1380376de046de902d3a21df968a8
Айым Оспанова <ajymospanova@Noutbuk-Ajym.local>
2026-03-17 01:37:52 +0500

    2

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Backend работает");
+});
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
