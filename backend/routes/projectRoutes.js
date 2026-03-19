a345cedbe1deda090445462cd5c08c9a272f3df3
Айым Оспанова <ajymospanova@Noutbuk-Ajym.local>
2026-03-19 14:02:29 +0500

    55

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const projectController = require("../controllers/projectController");
 const auth = require("../middleware/auth");
 
-router.get("/", projectController.getProjects);
+router.get("/", auth, projectController.getProjects);
 router.post("/", auth, projectController.createProject);
 router.put("/:id", auth, projectController.updateProject); // Для Update
 router.delete("/:id", auth, projectController.deleteProject); // Для Delete
