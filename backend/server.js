df442834bc535b4c64ec3e0b5ed7dc37741f5d93
Айым Оспанова <ajymospanova@Noutbuk-Ajym.local>
2026-04-03 23:52:31 +0500

    fix

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,6 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
