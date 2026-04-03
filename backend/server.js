c3a02d648e260951c4ebc1751853755f97045fdb
Айым Оспанова <ajymospanova@Noutbuk-Ajym.local>
2026-04-03 23:48:37 +0500

    frontend build

--- backend/server.js
+++ backend/server.js
@@ -34,3 +34,10 @@ const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+});
